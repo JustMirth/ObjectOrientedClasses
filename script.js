50409541774d5f90dbb3d99e698179c785b8c114
Mitchell Callaghan <mitchy1898@gmail.com>
2024-12-12 11:35:06 -0500

    Added true

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ Product.applyDiscount(store.inventory, 0.15);
 console.log("Inventory Value After Discount: $", store.getInventoryValue().toFixed(2));
 
 const foundProduct = store.findProductByName("Apple");
-    if (foundProduct) {
+    if (foundProduct = true) {
         console.log("Details of Searched Product:", foundProduct.toString());
     } else {
         console.log("Product not found.");
