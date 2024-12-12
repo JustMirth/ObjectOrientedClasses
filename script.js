c2a6f9e9d6cfa6f3df6637295cb9adb0a58e121f
Mitchell Callaghan <mitchy1898@gmail.com>
2024-12-12 10:40:51 -0500

    Renamed function to 'toString' and made override one for perishables

--- script.js
+++ script.js
@@ -7,13 +7,16 @@ class ProductProperties {
     getTotalValue() {
         return this.price * this.quantity
     }
-    stringProductProperties() {
-        return `Product: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}`
+    toString() {
+        return `Product: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}`;
     }
 }
 class PerishableProductProperties extends ProductProperties {
     constructor (name, price, quantity, expirationDate) {
         super(name, price, quantity);
         this.expirationDate = expirationDate;
+    }
+    toString() {
+        return `Product: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`;
     }
 }
