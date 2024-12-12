e616c835d0143fb03414a1d580653a4078ea1414
Mitchell Callaghan <mitchy1898@gmail.com>
2024-12-12 10:45:46 -0500

    Made two instances of perishable class and labelled part sections

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Part 1
 class ProductProperties {
     constructor(name, price, quantity) {
         this.name = name;
@@ -11,6 +12,7 @@ class ProductProperties {
         return `Product: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}`;
     }
 }
+//Part 2
 class PerishableProductProperties extends ProductProperties {
     constructor (name, price, quantity, expirationDate) {
         super(name, price, quantity);
@@ -20,3 +22,5 @@ class PerishableProductProperties extends ProductProperties {
         return `Product: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}, Expiration Date: ${this.expirationDate}`;
     }
 }
+const Perishable1 = PerishableProductProperties ("Apple", 2.50, 50, "2024-12-18");
+const Perishable2 = PerishableProductProperties ("Milk", 1.50, 10, "2024-12-31");
